d931ce40a66dfede07fbace13eabc3df450573eb
calvin chan <chancch70@gmail.com>
2021-01-09 00:22:49 +0800

    FaqSection

---
+++ src/components/faqSection.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How to Start</h4>
+        <div className="answer">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsam
+            tempora aperiam perferendis explicabo earum. Repellendus, ex sint
+            sed, esse modi incidunt ut unde accusantium expedita, labore
+            assumenda minima nemo.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>daily schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsam
+            tempora aperiam perferendis explicabo earum. Repellendus, ex sint
+            sed, esse modi incidunt ut unde accusantium expedita, labore
+            assumenda minima nemo.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Different payment</h4>
+        <div className="answer">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsam
+            tempora aperiam perferendis explicabo earum. Repellendus, ex sint
+            sed, esse modi incidunt ut unde accusantium expedita, labore
+            assumenda minima nemo.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What products do you order</h4>
+        <div className="answer">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ipsam
+            tempora aperiam perferendis explicabo earum. Repellendus, ex sint
+            sed, esse modi incidunt ut unde accusantium expedita, labore
+            assumenda minima nemo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,13 @@
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServiceSection";
+import FaqSection from "../components/faqSection";
+
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
