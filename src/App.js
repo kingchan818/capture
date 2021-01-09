152b8acdaf5ee4c88240edba4028f25f425b29ac
calvin chan <chancch70@gmail.com>
2021-01-09 19:58:31 +0800

    styleComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import AboutUS from "./pages/AboutUs";
-
+import GlobalStyle from "./components/GlobalStyle";
 function App() {
   return (
     <div>
       <h1>film</h1>
+      <GlobalStyle />
       <AboutUS />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,71 @@
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>We work to make</h2>
-        </div>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>we work to make</h2>
+          </Hide>
 
-        <div className="hide">
-          <h2>
-            your <span>dreams</span> come true
-          </h2>
-        </div>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
 
-        <div className="hide">
-          <h2>True</h2>
+          <Hide>
+            <h2>true</h2>
+          </Hide>
         </div>
         <p>
-          contact us for any photography or videography idea we make you come
-          true
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
         </p>
-      </div>
-      <div className="image">
-        <img src={home1} alt="cam man" />
-      </div>
-    </div>
+
+        <button> Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with cam" />
+      </Image>
+    </About>
   );
 };
+
+//styled Components
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
